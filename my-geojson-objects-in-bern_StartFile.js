9c6aa76e905d8418d3f72ec868a46042d00e2bd7
Fireman <softart@gmx.de>
2015-10-30 09:36:18 +0100

    initial files for the Bern Education case

---
+++ my-geojson-objects-in-bern_StartFile.js
@@ -0,0 +1,40 @@
+var geojson = [
+
+{
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [7.464874499999951 , 46.9631123]
+    },
+    "properties": {
+      "title": "Stade de Suisse",
+      "description": "ehemals Wankdorf-Stadion, Sempachstrasse, Bern, Schweiz",
+      "marker-color": "#fc4353",
+      "marker-size": "medium",
+	  "marker-symbol": "soccer"
+    }
+}
+,
+{
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [7.442454699999985, 46.9440935]
+    },
+    "properties": {
+      "title": "BFH",
+      "description": "<a href=\"https://www.wirtschaft.bfh.ch/en/homepage_en.html\">Berner Fachhochschule</a><br>" +
+								"<img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/BFH_Fachbereich_Wirtschaft%2C_Geb%C3%A4ude_Br%C3%BCckenstrasse.jpg/220px-BFH_Fachbereich_Wirtschaft%2C_Geb%C3%A4ude_Br%C3%BCckenstrasse.jpg\" width=\"100\">",
+      "marker-color": "#fc4353",
+      "marker-size": "medium",
+	  "marker-symbol": "college"
+    }
+}
+];
+
+// Cool marker icons are here: https://www.mapbox.com/maki
+
+// Geocoding services are here:
+// http://www.fair-hotels.de/Google-Earth/geokoordinaten.php
+// http://www.gpsvisualizer.com/geocode 
+// http://rechneronline.de/geo-koordinaten/  
